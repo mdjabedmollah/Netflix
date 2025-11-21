c10cf1b97ff833873aa93658845f72ebf1ab67ca
mdjabedmollah <mdjabed2167@gmail.com>
2025-11-21 13:36:33 +0600

    token and cookies

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,5 +1,46 @@
 
 import { User } from "../model/userModel.js";
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcryptjs";
+export const Login=async(req,res)=>{
+    try {
+        const {email,password}=req.body;
+        if((!email || !password)){
+            return res.status(400).json({
+                success:false,
+                message:"All field required"
+            })
+        }
+        const user =await User.findOne({email})
+        if(!user){
+             return res.status(400).json({
+                success:false,
+                message:"User not found"
+            })
+        }
+        const isMath=await bcrypt.compare(password, user.password); 
+        if(!isMath){
+              return res.status(400).json({
+                success:false,
+                message:"Invalid username or password"
+            })
+        }
+        const tokenData={
+            id:user._id
+        }
+        const token =await jwt.sign(tokenData,process.env.screact_key,{expiresIn:"1d"})
+        return res.status(200).cookie("token",token,{httpOnly:true}).json({
+            success:true,
+            message:"Login successful",
+        })
+    } catch (error) {
+        console.log("error is login ")
+          return res.status(400).json({
+                success:false,
+                message:error.message
+            })
+    }
+}
 export const Register = async (req, res) => {
   try {
     const { fullname, email, password } = req.body;
@@ -16,12 +57,13 @@ export const Register = async (req, res) => {
         success: false,
       });
     }
-    const newUser = User.create({
+    const hash =await bcrypt.hash(password, 10);
+    const newUser =await User.create({
       fullname,
       email,
-      password,
+      password:hash
     });
-    await newUser.save()
+     await newUser.save()
     return res.status(200).json({
         success:true,
         newUser
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { DB } from './utils/Db.js'
+import router from './routes/user.Route.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 const app=express()
 const port=process.env.PORT || 8081
@@ -8,8 +10,11 @@ const port=process.env.PORT || 8081
 //Data base connetion 
  DB()
 //Middleware
-
-
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+//api
+app.use('/api/vi/user',router)
 app.listen(port,()=>{
 console.log(`server listen at port ${port}`)
 })
